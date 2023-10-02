dce60229d34f86d4539e598cf142c6870eac5344
Hande Gul Atasagun <84257643+hgatasagun@users.noreply.github.com>
2023-10-02 20:47:21 +0300

    app.py

--- app.py
+++ app.py
@@ -104,5 +104,6 @@ def predict():
 
     return render_template('index.html', prediction_text=prediction_text)
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
